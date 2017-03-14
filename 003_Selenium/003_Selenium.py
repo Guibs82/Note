5b20b5cc55fec7c318dba0232e2d4f5ba594cf9d
Guibs82 <805249506@qq.com>
2017-03-14 21:37:10 +0800

    Note

--- 003_Selenium/003_Selenium.py
+++ 003_Selenium/003_Selenium.py
@@ -1,15 +1,31 @@
 #encoding:utf-8
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import unittest
 import time
 
-driver = webdriver.Safari()
+from selenium import webdriver
 
-driver.get("http://www.douban.com")
-time.sleep(1)
+class douyuSelenium(unittest.TestCase):
+    # 初始化方法
+    def setUp(self):
+        self.driver = webdriver.Safari()
 
-driver.find_element_by_name("form_email").send_keys("805249506@qq.com")
+    # 具体测试用例方法, 一定要以 test 开头
+    def testDB(self):
+        self.driver.get("https://movie.douban.com/typerank?type_name=剧情&type=11&interval_id=100:90&action=")
+        # 向下滚动10000 像素
+        js = "document.body.scrollTop=10000"
+        time.sleep(3)
+        self.driver.save_screenshot('douban.png')
 
-time.sleep(10)
-driver.quit()
+        self.driver.execute_script(js)
+        time.sleep(15)
+
+        self.driver.save_screenshot('newdouban.png')
+
+    # 退出时清理方法
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main
